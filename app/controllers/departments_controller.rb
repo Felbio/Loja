effed9335ac099f280a847221d9f9ce1c1bcc157
Felipe Oliveira <felipe.oliveira@walljobs.com.br>
2023-09-15 10:39:36 -0300

    updates in department

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -26,7 +26,7 @@ class DepartmentsController < ApplicationController
 
     respond_to do |format|
       if @department.save
-        format.html { redirect_to department_url(@department), notice: "Department was successfully created." }
+        format.html { redirect_to departments_url, notice: "Departamento foi criado com sucesso!" }
         format.json { render :show, status: :created, location: @department }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +39,7 @@ class DepartmentsController < ApplicationController
   def update
     respond_to do |format|
       if @department.update(department_params)
-        format.html { redirect_to department_url(@department), notice: "Department was successfully updated." }
+        format.html { redirect_to department_url(@department), notice: "Departamento foi atualizado com successo!" }
         format.json { render :show, status: :ok, location: @department }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,7 +53,7 @@ class DepartmentsController < ApplicationController
     @department.destroy
 
     respond_to do |format|
-      format.html { redirect_to departments_url, notice: "Department was successfully destroyed." }
+      format.html { redirect_to departments_url, notice: "#{@department.name} foi excluído com sucesso!" }
       format.json { head :no_content }
     end
   end
--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,8 +1,8 @@
 class Produto < ApplicationRecord
   
-  belongs_to :department, optional: true, dependent: :destroy
+  belongs_to :department, dependent: :destroy
 
-  validates :price, :name, :quantity, presence: true
+  validates :price, :name, :quantity, :department, presence: true
   validates :name, length: {minimum: 4}
   validates :quantity, numericality: { only_integer: true }
   validates :description, length:{in: 5..200}
